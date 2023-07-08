b6c9a151f46ec83c755d37107c4f9466aa860c02
Onat <53895969+onattech@users.noreply.github.com>
2023-07-08 20:01:43 +0300

    First commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+    return (
+        <div className="p-4">
+            <Button variant="outline">Click Me</Button>
+        </div>
+    )
+}
