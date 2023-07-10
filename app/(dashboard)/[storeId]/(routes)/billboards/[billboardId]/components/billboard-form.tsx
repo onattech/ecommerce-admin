d1115706faced83a6acff3890f356a2de1a95b6a
Onat <53895969+onattech@users.noreply.github.com>
2023-07-10 21:01:20 +0300

    Added Data Table

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -69,7 +69,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
             setLoading(true)
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
             router.refresh()
-            router.push("/")
+            router.push(`/${params.storeId}/billboards`)
             toast.success("Billboard deleted.")
         } catch (error) {
             toast.error("Make sure you removed all categories using this billboard first.")
@@ -132,7 +132,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
                     </Button>
                 </form>
             </Form>
-            <Separator />
         </>
     )
 }
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,20 +6,31 @@ import { useParams, useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     const router = useRouter()
     const params = useParams()
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title="Billboards (0)" description="Manage billboards for your store" />
+                <Heading title={`Billboards ${data.length}`} description="Manage billboards for your store" />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
+            <DataTable searchKey="label" columns={columns} data={data} />
+            <Heading title="API" description="API calls for Billboards" />
+            <Separator />
+            <ApiList entityIdName="billboardId" entityName="billboards" />
         </>
     )
 }
